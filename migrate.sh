39355ca75455a96893e99dbbb14e652ec370ecca
Zhang Zhengwen <zhengwen.zhang@afanty-space.com>
2018-09-12 16:15:30 +0800

    update time: Wed Sep 12 16:15:30 CST 2018

--- migrate.sh
+++ migrate.sh
@@ -7,7 +7,7 @@ function backup(){
     cp -f "$HOME/.vimrc" "$HOME/Documents/config/vimrc"
     cp -f "$HOME/.zshrc" "$HOME/Documents/config/zshrc"
     cp -f "$HOME/.zimrc" "$HOME/Documents/config/zimrc"
-    cp -a "$HOME/.vim/colors" "$HOME/Documents/config/vim/colors/"
+    cp -R "$HOME/.vim/colors/" "$HOME/Documents/config/vim/colors/"
     git add .
     git commit -a -m "update time: $(date)"
     git push origin HEAD
@@ -19,7 +19,7 @@ function restore(){
     cp -f "$HOME/Documents/config/vimrc" "$HOME/.vimrc"
     cp -f "$HOME/Documents/config/zshrc" "$HOME/.zshrc"
     cp -f "$HOME/Documents/config/zimrc" "$HOME/.zimrc"
-    cp -a "$HOME/Documents/config/vim/colors/gruvbox.vim" "$HOME/.vim/colors/gruvbox.vim"
+    cp -R "$HOME/Documents/config/vim/colors/" "$HOME/.vim/colors/"
 }
 
 if [ $# -ne 1 ]; then
