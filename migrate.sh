33631fd1a14e12a9413a452c9717482909f94297
Lochuan <fcluochuan@gmail.com>
2019-01-27 21:23:33 +0800

    Mac update time: Sun Jan 27 21:23:33 CST 2019

--- migrate.sh
+++ migrate.sh
@@ -1,36 +1,72 @@
 #!/bin/bash
 set -e
 
+OS=$(uname)
+
+function darwin_b(){
+  echo 'Mac config backup..'
+  cp -f "$HOME/.zshrc" "$HOME/Documents/config/mac-zshrc"
+  cp -f "$HOME/.zimrc" "$HOME/Documents/config/mac-zimrc"
+  cp -Rf "$HOME/.SpaceVim.d" "$HOME/Documents/config/"
+  git add .
+  git commit -a -m "Mac update time: $(date)"
+  git push origin HEAD
+}
+
+function linux_b(){
+  echo 'Linux config backup..'
+  cp -f "$HOME/.zshrc" "$HOME/Documents/config/linux-zshrc"
+  cp -f "$HOME/.zimrc" "$HOME/Documents/config/Linux-zimrc"
+  cp -Rf "$HOME/.SpaceVim.d" "$HOME/Documents/config/"
+  git add .
+  git commit -a -m "Linux update time: $(date)"
+  git push origin HEAD
+}
+
+function darwin_r(){
+  echo 'Mac config restore...'
+  git pull
+  cp -f "$HOME/Documents/config/mac-zshrc" "$HOME/.zshrc"
+  cp -f "$HOME/Documents/config/mac-zimrc" "$HOME/.zimrc"
+  cp -Rf "$HOME/Documents/config/.SpaceVim.d" "$HOME/"
+}
+
+function linux_r(){
+  echo 'Linux config restore...'
+  git pull
+  cp -f "$HOME/Documents/config/linux-zshrc" "$HOME/.zshrc"
+  cp -f "$HOME/Documents/config/linux-zimrc" "$HOME/.zimrc"
+  cp -Rf "$HOME/Documents/config/.SpaceVim.d" "$HOME/"
+}
+
 function backup(){
-    echo 'backup...'
-    cp -f "$HOME/.config/nvim/init.vim" "$HOME/Documents/config/init.vim"
-    cp -f "$HOME/.zshrc" "$HOME/Documents/config/zshrc"
-    cp -f "$HOME/.zimrc" "$HOME/Documents/config/zimrc"
-    git add .
-    git commit -a -m "update time: $(date)"
-    git push origin HEAD
+  if [[ $OS == Darwin ]]; then
+    darwin_b
+  elif [[ $OS == Linux ]]; then
+    linux_b
+  fi
 }
 
 function restore(){
-    echo 'restore...'
-    git pull
-    cp -f "$HOME/Documents/config/init.vim" "$HOME/.config/nvim/init.vim"
-    cp -f "$HOME/Documents/config/zshrc" "$HOME/.zshrc"
-    cp -f "$HOME/Documents/config/zimrc" "$HOME/.zimrc"
+  if [[ $OS == Darwin ]]; then
+    darwin_r
+  elif [[ $OS == Linux ]]; then
+    linux_r
+  fi
 }
 
 if [ $# -ne 1 ]; then
-    echo 'Usage: migrate [backup | restore]'
+  echo 'Usage: migrate [backup | restore]'
 else
-    case "$1" in
-        'backup')
-            backup
-            ;;
-        'restore')
-            restore
-            ;;
-        *)
-            echo 'Usage: migrate [backup | restore]'
-            ;;
-    esac
+  case "$1" in
+    'backup')
+      backup
+      ;;
+    'restore')
+      restore
+      ;;
+    *)
+      echo 'Usage: migrate [backup | restore]'
+      ;;
+  esac
 fi
